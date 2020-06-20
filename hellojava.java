6ed535820b120f1af374bbdf3b3b698361d52fdc
xiaozhikun333 <253898549@qq.com>
2020-06-20 16:26:45 +0800

    add

---
+++ hellojava.java
@@ -0,0 +1,20 @@
+public class hellojava{
+	public static void main(String[] args){
+	System.out.println("姓名：肖志昆");
+	System.out.println("性别：男");
+	System.out.println("籍贯：河南");
+	System.out.println("住址：河南信阳市潢川县");
+	System.out.println("    *                       *    ");
+	System.out.println("*        *  I love you  *      *");
+	System.out.println("    *                         *");
+	System.out.println("      *                     *");
+	System.out.println("        *                 *");
+	System.out.println("          *             *");
+	System.out.println("            *         *");
+	System.out.println("              *     *");
+	System.out.println("                 *");
+	}	
+
+
+
+}
