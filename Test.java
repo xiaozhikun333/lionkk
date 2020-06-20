5d54de3083f3a99d993f6f018e5ef7776f369336
xiaozhikun333 <253898549@qq.com>
2020-06-20 14:46:28 +0800

    add file

---
+++ Test.java
@@ -0,0 +1,7 @@
+class Test{
+	public static void main(String[] args){
+	System.out.println("hello world!");
+	System.out.println("hello world!");
+	System.out.println("ÄãºÃ!");
+	}
+}
---
+++ lizi.java
@@ -0,0 +1,7 @@
+Class lizi{
+	public static void main(String[] args){
+	System.out.println("hello world!");
+	System.out.println("hello world!");
+	System.out.println("ÄãºÃ!");
+	}
+}
